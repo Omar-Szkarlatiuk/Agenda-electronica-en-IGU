4cf2ae82f5d38629d8e1ecd6df4fda246373f461
Omar-Szkarlatiuk <89821003+Omar-Szkarlatiuk@users.noreply.github.com>
2021-09-13 20:37:28 -0300

    Add files via upload
    Agenda electrónica escrita en el IGU sin base de datos o POO.
    -Admite 10 entradas
    -los botones << y >>, para pasar de una entrada a otra, se deshabilitan cuando llegan al tope.
    -El botón Guardar almacena los datos
    -El campo DNI solo admite números, y tiene un tope de 10. El campo teléfono admite 8 números
    -El campo F. Nac. admite la fecha en formato dd/mm/aaaa o dd-mm-aaaa

---
+++ TPO_1/src/logica/AgendaLogica.java
@@ -0,0 +1,21 @@
+package logica;
+import igu.AgendaPantalla; //carga el package del IGU
+
+public class AgendaLogica {
+    public static void main(String[] args) {
+        AgendaPantalla pantalla = new AgendaPantalla(); //crea una pantalla en base al IGU
+        pantalla.setVisible(true);  //hace que la pantalla sea visible
+        pantalla.setLocationRelativeTo(null); //hace que la pantalla se ubique en el medio
+        
+        int listaDNI[] = new int [10];
+        String listaNom[] = new String [10];
+        String listaApellido[] = new String [10];
+        String listaDire[] = new String [10];
+        int listaTel[] = new int [10];
+        String listaCumple[] = new String[10];
+        
+        
+        
+    }
+    
+}
